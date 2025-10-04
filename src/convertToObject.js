51379ab929f1505ed59e085c601efa9781ac2a9c
Daniel <dania.szumski@gmail.com>
2025-10-04 12:48:49 +0200

    Done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,29 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   // write your code here
+  const res = {};
+  const lines = sourceString.split('\n');
+
+  let currentProperty = null;
+
+  for (let line of lines) {
+    line = line.trim();
+    if (line === '') continue;
+
+    if (line.includes(':')) {
+      const [property, value] = line.split(':');
+      currentProperty = property.trim();
+      res[currentProperty] = value.trim();
+    } else {
+      res[currentProperty] += ` ${line.trim()}`;
+    }
+  }
+
+  return res;
+
 }
 
 module.exports = convertToObject;
